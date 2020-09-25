3cbea4e667b79f945a76255fa14d1f07b3b58401
Branislav Totic <branislav.totic@decem.co>
2020-09-25 13:37:25 +0200

    Add server and add socket.io connection

---
+++ src/ClientComponent.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:3001";
+
+export default function ClientComponent() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+    //
+  }, []);
+
+  if (!response) {
+    return (<p>Loading...</p>)
+  }
+
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+  );
+}
